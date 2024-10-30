68dd13841f70aa11d55e39ecb782b718fa6698da
committ <89640845+spaceComplexitys@users.noreply.github.com>
2024-10-30 17:54:12 -0400

    SQ lite connection complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <h1>Hello, World!</h1>;
+// export default function Page() {
+//   return <h1>Hello, World!</h1>;
+// }
+
+import { db } from "@/db";
+import { postsTable, usersTable } from "@/db/schema";
+
+export default async function Home() {
+  const post = await db.query.postsTable.findMany();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono">
+        <form
+          action={async () => {
+            "use server";
+            await db.insert(usersTable).values({
+              id: 2,
+              age: 20,
+              email: "test@example.com",
+              name: "bob",
+            });
+            await db.insert(postsTable).values({
+              title: "please subscribe",
+              content: "yolo",
+              userId: 2,
+            });
+          }}
+        >
+          <button>submit</button>
+        </form>
+        {post.map(post => (
+          <div>{post.title}</div>
+        ))}
+      </div>
+    </main>
+  );
 }
