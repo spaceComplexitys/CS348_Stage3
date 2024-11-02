5357d32ca54f21ba0ce1c71f9cf28e55d421dfcb
committ <89640845+spaceComplexitys@users.noreply.github.com>
2024-11-02 16:15:15 -0400

    Removed Unnecessary files
    I was trying to get subroutes but didn't work

--- src/app/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-const About: NextPage = () => {
-  return <h1>About Page</h1>;
-};
-
-export default About;
--- src/app/pages/blog/index.tsx
+++
@@ -1,8 +0,0 @@
-// pages/blog/index.tsx
-import { NextPage } from 'next';
-
-const Blog: NextPage = () => {
-  return <h1>Blog Index Page</h1>;
-};
-
-export default Blog;
