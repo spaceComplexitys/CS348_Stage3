6f3a9fe8580d2943f7016733e6977552ccef1c24
committ <89640845+spaceComplexitys@users.noreply.github.com>
2024-11-12 20:33:44 -0500

    Fixed Timestamp to be current time

--- src/app/api/addTransaction/route.ts
+++ src/app/api/addTransaction/route.ts
@@ -6,18 +6,17 @@ import type { NextRequest } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-
     const body = await request.json();
 
     // Use the body data to define the data object
     const data = {
       user_id: body.user_id || 10000000, // default value if not provided
-      date: body.date || "1/1/1",
-      payee: body.payee || "Default",
-      category: body.category || "Default",
-      memo: body.memo || "Default",
+      date: body.date,
+      payee: body.payee || 'Default',
+      category: body.category || 'Default',
+      memo: body.memo || 'Default',
       outflow: body.outflow || 0,
-      inflow: body.inflow || 0
+      inflow: body.inflow || 0,
     };
 
     // Insert the transaction data into the database
@@ -26,12 +25,9 @@ export async function POST(request: NextRequest) {
     // Return the newly created transaction in JSON format
     return NextResponse.json(newTransaction);
   } catch (error) {
-    console.error("Error creating transaction:", error);
+    console.error('Error creating transaction:', error);
 
     // Return an error response in case of failure
-    return NextResponse.json(
-      { error: 'Failed to create transaction' },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: 'Failed to create transaction' }, { status: 500 });
   }
 }
